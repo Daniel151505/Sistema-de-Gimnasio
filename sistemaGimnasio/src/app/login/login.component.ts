1f9e11e00e0e39048f41f4287f55ff81b5e86427
Daniel151505 <daniel150520011@gmail.com>
2021-01-26 22:23:10 -0500

    Validando Email y Password

--- sistemaGimnasio/src/app/login/login.component.ts
+++ sistemaGimnasio/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+ formularioLogin!: FormGroup;
+
+  constructor(private creadorFormulario: FormBuilder ) { }
 
   ngOnInit(): void {
+
+    this.formularioLogin = this.creadorFormulario.group({
+      email: ['',Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      password: ['', Validators.required]
+    });
+
   }
 
 }
