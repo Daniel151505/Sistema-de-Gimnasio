376994bb66d0a407b3bc33a6723c17b4fcac5056
Daniel151505 <daniel150520011@gmail.com>
2021-01-27 23:05:57 -0500

    Agregando ruta listado-cliente

--- sistemaGimnasio/src/app/app-routing.module.ts
+++ sistemaGimnasio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListadoClienteComponent } from './listado-cliente/listado-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'listado-cliente', component: ListadoClienteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
