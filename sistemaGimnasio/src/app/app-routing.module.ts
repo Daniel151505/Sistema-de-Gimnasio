b5cbb7a96fec61694df017454490022753e0eec6
Daniel151505 <daniel150520011@gmail.com>
2021-01-28 21:30:54 -0500

    Agregando ruta Agregar Cliente

--- sistemaGimnasio/src/app/app-routing.module.ts
+++ sistemaGimnasio/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.component';
 import { ListadoClienteComponent } from './listado-cliente/listado-cliente.component';
 
 const routes: Routes = [
   {
     path: 'listado-cliente', component: ListadoClienteComponent
+  },
+  {
+    path: 'agregar-cliente', component: AgregarClienteComponent
   }
 ];
 
