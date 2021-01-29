d97b913f5eb46f4e84a1cec685d13f0af774ceac
Daniel151505 <daniel150520011@gmail.com>
2021-01-28 21:20:42 -0500

    Leyendo datos Firebase

--- sistemaGimnasio/src/app/listado-cliente/listado-cliente.component.ts
+++ sistemaGimnasio/src/app/listado-cliente/listado-cliente.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore'
-
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
-  selector: 'app-listado-cliente',
+  selector: 'app-listado-clientes',
   templateUrl: './listado-cliente.component.html',
   styleUrls: ['./listado-cliente.component.scss']
 })
 export class ListadoClienteComponent implements OnInit {
-  cliente: any [] = new Array<any>();
 
-  constructor(private db:AngularFirestore) { }
+  clientes: any[] = new Array<any>();
+  constructor(private db: AngularFirestore) { }
 
-  ngOnInit(): void {
-
-  this.db.collection('clientes').valueChanges().subscribe((resultado)=>{
-    this.cliente = resultado;
-  })
-
+  ngOnInit() {
+    this.clientes.length = 0;
+    this.db.collection('clientes').get().subscribe((resultado) => {
+     
+      resultado.docs.forEach((item) => {
+       
+        let cliente:any = item.data();
+        cliente.id = item.id;
+        cliente.rf = item.ref;
+        this.clientes.push(cliente)
+      });
+    });
   }
 
 }
